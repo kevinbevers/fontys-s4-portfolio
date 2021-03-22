f0b6f4caace6cac35b3921065d0972b785273436
Kevin Bevers <kevin.bevers@hotmail.com>
2021-03-22 15:36:59 +0100

    updated date

--- src/components/Promotion.js
+++ src/components/Promotion.js
@@ -660,7 +660,7 @@ I made a setup to test the things I’m trying to built and this setup is probab
     className="vertical-timeline-element"
     contentStyle={{ background: 'rgb(220,220,220)' }}
     contentArrowStyle={{ borderRight: '7px solid  rgb(220,220,220)' }}
-    date="18 March 2021"
+    date="22 March 2021"
     iconStyle={{ background: 'rgb(220,220,220)' }}
     icon={<FaPencilRuler />}
   >
